eef26cfde9cfe62bb39c4509d74e6de532f86c8c
Šimon Brávek <simonbravek17@gmail.com>
2023-09-13 12:01:50 +0000

    03

--- 01.py
+++ 01.py
@@ -1,1 +1,8 @@
-print("Hello")
+def deleni(delenec, delitel):
+    vysledek = delenec / delitel
+    return vysledek
+
+delenec = int(input("Můj dělenec je: "))
+delitel = int(input("Můj dělitel je: "))
+
+print(deleni(delenec, delitel))
